88bc0d98c1dfc264c0c8c0deb44c6852a0a4f9a2
Erich Raschle <erichraschle@gmail.com>
2025-03-30 00:23:31 +0100

    chore: Add initial project files
    Created a .gitignore to exclude Python-generated files
    and virtual environments. Added README.md for project
    documentation and a basic env_backup_restore.py script
    to demonstrate functionality.

---
+++ env_backup_restore.py
@@ -0,0 +1,13 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "1password",
+#     "click",
+# ]
+# ///
+def main():
+    print("Hello from env-setup!")
+
+
+if __name__ == "__main__":
+    main()
